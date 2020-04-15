1b29b90119c1e18f55b5780213ae3165fb083589
Alexinder Virdee <alex.virdee08@gmail.com>
2020-04-15 08:08:49 -0400

    Add fontawesome icons + ecommerce store buttons

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAmazon, faEbay } from '@fortawesome/free-brands-svg-icons'
+
+import Hero from '../../images/epoxy-swirl.jpg';
 
 function Landing(props) {
     return (
         <div className="landing-content">
             <h3>Surface Monkey</h3>
-            <h5>Main Content</h5>
+            <h5>Discover Our Products</h5>
+            <a className="waves-effect waves-light btn-large store-btn black"><FontAwesomeIcon icon={faAmazon} size="2x"></FontAwesomeIcon></a>
+            <a className="waves-effect waves-light btn-large store-btn black"><FontAwesomeIcon icon={faEbay} size="2x"></FontAwesomeIcon></a>
             <section className="hero">
-                
             </section>
         </div>
     )
