b10b9bbfec75e8954571fef8091ed722e07ef6f3
Alexinder Virdee <alex.virdee08@gmail.com>
2020-05-19 12:30:51 -0400

    Side navigation trigger

--- src/components/layout/navigation/Navbar.jsx
+++ src/components/layout/navigation/Navbar.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
+import M from 'materialize-css';
+import "materialize-css/dist/css/materialize.min.css";
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 import './navbar.styles.css';
 
 import logo from '../../../images/method-draw-image.svg';
 
-function Navbar(props) {
-  return (
-    <div>
-      <nav className="white">
-        <div className="nav-wrapper">
-        <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-          <Link to="/" className="brand-logo">
-            <img className="nav-logo" alt="logo" src={logo}></img>
-          </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down nav-list">
-            <li>
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
+class Navbar extends React.Component {
+  // componentDidMount() {
+  //   const options = {
+  //     inDuration: 250,
+  //     outDuration: 200
+  //   }
+
+  //   M.Sidenav.init(this.sidenav);
+
+  //   let instance = M.Sidenav.getInstance(this.Sidenav);
+  //   instance.open();
+  //   console.log(instance.isOpen);
+  // };
+
+  render() {
+    return (
+      <div>
+        <nav className="white">
+          <div className="nav-wrapper">
+          <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+            <Link to="/" className="brand-logo">
+              <img className="nav-logo" alt="logo" src={logo}></img>
+            </Link>
+            <ul className="right hide-on-med-and-down nav-list">
+              <li>
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <ul className="sidenav" id="nav-mobile">
+          <li><Link className="nav-link" to="/about">About</Link></li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 Navbar.propTypes = {};
