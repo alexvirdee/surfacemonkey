49b6499f305499c5d15624a55c76a158935b53ab
Alexinder Virdee <alex.virdee08@gmail.com>
2020-05-19 11:57:13 -0400

    Product list and product components

---
+++ src/components/product-list/Product-list.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Product } from '../product/Product';
+
+import './product-list/styles.css';
+
+export const ProductList = () => (
+    <div className="product-list">
+        <Product />
+    </div>
+)
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './product.styles.css';
+
+export const Product = () => (
+    <div className="product-container">
+        <h3>PRODUCT</h3>
+    </div>
+)
